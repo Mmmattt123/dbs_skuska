6dce6ef0b6c9f32d8edf5a9dac4e335ed2ca0c02
Mmmattt123 <matej.janeba@gmail.com>
2017-03-21 09:20:32 +0100

    spravene cards na frontende

--- webapp/urls.py
+++ webapp/urls.py
@@ -5,9 +5,4 @@ urlpatterns = [
 	url(r'^$', views.index, name = 'index'),
 	url(r'^insert', views.insert),
 	url(r'^delete_user', views.delete),
-<<<<<<< HEAD
-
-=======
->>>>>>> 75eed474ab8a3724166f8070f62621ac8b04c144
-
 ]
